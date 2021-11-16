e8db9479a32ffd6cd8a6adfa1263f9e50c999f04
waxmoth <waxmoth@qq.com>
2021-11-16 15:05:38 +0800

    Fix the process cannot restart after the Go file changed

---
+++ src/middlewares/json_middleware.go
@@ -0,0 +1,10 @@
+package middlewares
+
+import "github.com/gin-gonic/gin"
+
+func JSONMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+		c.Next()
+	}
+}
--- src/routers/router.go
+++ src/routers/router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"image-functions/src/api/images"
+	"image-functions/src/middlewares"
 )
 
 func Routers() *gin.Engine {
@@ -10,6 +11,7 @@ func Routers() *gin.Engine {
 
 	r := gin.Default()
 	r.GET("/image", images.GetImage)
+	r.Use(middlewares.JSONMiddleware())
 
 	return r
 }
