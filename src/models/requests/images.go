df42ec39487aa07c4b6672e60847a37487538de9
Weixian Ding <waxmoth@qq.com>
2023-07-01 20:22:55 +0800

    Add method to resize and crop image (#4)

--- src/models/requests/images.go
+++ src/models/requests/images.go
@@ -2,7 +2,10 @@ package requests
 
 // GetImage the request object for GetImage API
 type GetImage struct {
-	URL    string `form:"url" json:"url" binding:"required"`
-	Width  int16  `form:"width" json:"width"`
-	Height int16  `form:"height" json:"height"`
+	URL       string `form:"url" json:"url" binding:"required"`
+	Type      string `form:"type" json:"type"`
+	Width     int    `form:"width" json:"width"`
+	Height    int    `form:"height" json:"height"`
+	PositionX int    `form:"x" json:"x"`
+	PositionY int    `form:"y" json:"y"`
 }
