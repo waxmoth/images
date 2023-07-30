d651a6a1f09622284100cc05e6119a3c07e1e045
waxmoth <waxmoth@qq.com>
2023-07-30 14:43:16 +0800

    Code clean up

--- src/middlewares/AuthMiddleware.go
+++ src/middlewares/AuthMiddleware.go
@@ -13,15 +13,16 @@ import (
 func AuthMiddleware() gin.HandlerFunc {
 	return func(ct *gin.Context) {
 		user := ct.GetHeader(consts.AuthUser)
+		authKey := os.Getenv("AUTH_KEY" + "_" + strings.ToUpper(user))
 		reqToken := ct.GetHeader("Authorization")
-		if reqToken == "" || user == "" || !strings.Contains(reqToken, "Bearer ") {
+		if reqToken == "" || user == "" || authKey == "" || !strings.Contains(reqToken, "Bearer ") {
 			ct.Data(http.StatusUnauthorized, "application/json", []byte("Unauthorized"))
 			ct.Abort()
 			return
 		}
 		tokenString := reqToken[len("Bearer "):]
 		var authService auth.Auth = &auth.JWTService{
-			Key: os.Getenv("AUTH_KEY" + "_" + strings.ToUpper(user)),
+			Key: authKey,
 		}
 		token, err := authService.Decode(tokenString)
 		if err != nil {
