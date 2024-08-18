eea39235cb37f5c8633ab6c0404040a273a03163
waxmoth <waxmoth@qq.com>
2024-08-18 21:52:49 +0800

    Code clean up

--- src/main.go
+++ src/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"image-functions/src/routers"
+	"log"
 )
 
 func main() {
 	r := routers.Routers()
 	r.ForwardedByClientIP = true
-	err := r.SetTrustedProxies([]string{"127.0.0.1"})
-	err = r.Run(":8080")
+	if r.SetTrustedProxies([]string{"127.0.0.1"}) != nil {
+		log.Fatal("Failed set trusted proxies")
+	}
+	err := r.Run(":8080")
 	if err != nil {
 		panic(err)
 	}
