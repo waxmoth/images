68735095cfefefc4dc53b921c3eb85554619629b
waxmoth <waxmoth@qq.com>
2024-09-14 17:36:01 +0800

    IMG-150 Fix the path injection security issue

--- src/services/storage/local_service.go
+++ src/services/storage/local_service.go
@@ -1,20 +1,29 @@
 package storage
 
 import (
+	"errors"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
-// LocalService handle files in local
+const safeDir = "/tmp/"
+
+// LocalService handle files in local storage
 type LocalService struct {
 	LocalFolder string
 }
 
-// Initial create s3 service from the configures
+// Initial create local storage service from the configures
 func (storageServ *LocalService) Initial() error {
 	if !storageServ.BucketExists(storageServ.LocalFolder) {
 		log.Printf("LocalService|Initial|No bucket: %s", storageServ.LocalFolder)
-		err := os.MkdirAll(storageServ.LocalFolder, os.ModePerm)
+		resolvedFolder, err := storageServ.resolveFileName(storageServ.LocalFolder)
+		if err != nil {
+			return err
+		}
+		err = os.MkdirAll(resolvedFolder, os.ModePerm)
 		if err != nil {
 			return err
 		}
@@ -22,18 +31,26 @@ func (storageServ *LocalService) Initial() error {
 	return nil
 }
 
-// GetFile get file buffer from s3, return error if you cannot get it
+// GetFile get file buffer from local storage, return error if you cannot get it
 func (storageServ *LocalService) GetFile(fileName string) ([]byte, error) {
-	content, err := os.ReadFile(storageServ.LocalFolder + "/" + fileName)
+	resolvedFileName, err := storageServ.resolveFileName(storageServ.LocalFolder + "/" + fileName)
+	if err != nil {
+		return nil, err
+	}
+	content, err := os.ReadFile(resolvedFileName)
 	if err != nil {
 		return nil, err
 	}
 	return content, err
 }
 
-// UploadFile upload the file into s3 service
+// UploadFile upload the file into local storage service
 func (storageServ *LocalService) UploadFile(buf []byte, fileName string) bool {
-	err := os.WriteFile(storageServ.LocalFolder+"/"+fileName, buf, 0644)
+	resolvedFileName, err := storageServ.resolveFileName(storageServ.LocalFolder + "/" + fileName)
+	if resolvedFileName == "" || err != nil {
+		return false
+	}
+	err = os.WriteFile(resolvedFileName, buf, 0644)
 	if err != nil {
 		log.Printf("LocalService|UploadFile|Cannot upload the file %s|Error: %s", fileName, err)
 	}
@@ -42,7 +59,11 @@ func (storageServ *LocalService) UploadFile(buf []byte, fileName string) bool {
 
 // BucketExists check if the bucket exists
 func (storageServ *LocalService) BucketExists(folder string) bool {
-	if info, err := os.Stat(folder); os.IsNotExist(err) || !info.IsDir() {
+	resolvedFolder, err := storageServ.resolveFileName(folder)
+	if err != nil {
+		return false
+	}
+	if info, err := os.Stat(resolvedFolder); os.IsNotExist(err) || !info.IsDir() {
 		return false
 	}
 	return true
@@ -50,6 +71,21 @@ func (storageServ *LocalService) BucketExists(folder string) bool {
 
 // FileExists check if the file exists
 func (storageServ *LocalService) FileExists(fileName string) bool {
-	_, err := os.Stat(storageServ.LocalFolder + "/" + fileName)
+	resolvedFileName, err := storageServ.resolveFileName(storageServ.LocalFolder + "/" + fileName)
+	if err != nil {
+		return false
+	}
+
+	_, err = os.Stat(resolvedFileName)
 	return err == nil
+}
+
+// resolveFileName resolve the file name, in case the path injection issue
+func (storageServ *LocalService) resolveFileName(fileName string) (string, error) {
+	resolvedFileName, err := filepath.Abs(filepath.Join(safeDir, fileName))
+	if err != nil || !strings.HasPrefix(resolvedFileName, safeDir) {
+		return "", errors.New("invalid file name")
+	}
+
+	return resolvedFileName, nil
 }
